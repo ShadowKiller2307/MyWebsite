6d4a148fa366f498e5756391dae30233c44cb4ae
ShadowKiller2307 <ShadowKiller2307@users.noreply.github.com>
2019-12-26 17:34:40 +0100

    ticTacToe

--- ticTacToe.js
+++ ticTacToe.js
@@ -5,7 +5,8 @@ var Players = ["X", "O"];
 var currentPlayer = 0;
 var winnigSlots = [];
 var added = false;
-var bot = false;
+var startingPlayer = 0;
+var difficulties = ["Off", "Random", "Easy", "Hard"], difficulty = 0;
 var locked = false;
 
 drawBoard();
@@ -30,17 +31,48 @@ function drawBoard() {
     board.moveTo(0, canvas.height / 3 * 2);
     board.lineTo(canvas.width, canvas.height / 3 * 2);
     board.stroke();
+    document.getElementById("startingPlayerButton").innerHTML = "Starting Player: " + Players[startingPlayer];
+    document.getElementById("difficultyButton").innerHTML = "Difficulty of Bot: " + difficulties[difficulty];
 }
 
 function resetBoard() {
     boardState = ["", "", "", "", "", "", "", "", ""];
-    currentPlayer = 0;
+    currentPlayer = startingPlayer;
     for (i = 1; i < 10; i++) {
         document.getElementById("slot" + i).innerHTML = "";
         document.getElementById("slot" + i).setAttribute("style", "background-color: rgba(240, 248, 255, 0)");
     }
     winnigSlots = [];
     locked = false;
+    if (difficulty && startingPlayer == 1) {
+        currentPlayer = 1;
+        calculateBotSlot();
+    }
+}
+
+function changeStartingPlayer() {
+    if (startingPlayer) {
+        startingPlayer = 0;
+    } else {
+        startingPlayer = 1;
+    }
+    if (isBoardEmpty()) {
+        currentPlayer = startingPlayer;
+    }
+    document.getElementById("startingPlayerButton").innerText = "Starting Player: " + Players[startingPlayer];
+}
+
+function changeDifficulty() {
+    if (isBoardEmpty()) {
+        difficulty = difficulty < difficulties.length - 1 ? difficulty + 1 : 0;
+        document.getElementById("difficultyButton").innerHTML = "Difficulty of Bot: " + difficulties[difficulty];
+        if (difficulty && startingPlayer == 1) {
+            currentPlayer = 1;
+            calculateBotSlot();
+        }
+    } else {
+        alert("Finish the current game before you can change the difficulty!");
+    }
 }
 
 function slot(slotNmbr) {
@@ -52,16 +84,18 @@ function slot(slotNmbr) {
 }
 
 function calculateWinner() {
+    locked = true;
     if (calculateRows() || calculateColums() || calculateDiagonals()) {
-        locked = true;
         setTimeout(function () { resetBoard(); }, 2000);
         drawWinningSlots();
     } else {
         if (boardState.includes("")) {
             if (currentPlayer) {
                 currentPlayer = 0;
+                locked = false;
             } else {
                 currentPlayer = 1;
+                calculateBotSlot();
             }
         } else { //if the board is tied
             locked = true;
@@ -73,8 +107,40 @@ function calculateWinner() {
     }
 }
 
+function calculateBotSlot() {
+    locked = true; // lock the board for the Bot to plan his move
+    if (difficulty != 0) {
+        var index = boardState.indexOf("");
+        var possibleSlots = [];
+        while (index != -1) {
+            possibleSlots.push(index);
+            index = boardState.indexOf("", index + 1);
+        }
+        var slotNmbr;
+        switch (difficulty) {
+            case 1:
+                slotNmbr = possibleSlots[Math.floor(Math.random() * possibleSlots.length)];
+                break;
+            case 2:
+
+                break;
+            case 3:
+                break;
+        }
+        document.getElementById("slot" + (slotNmbr + 1)).innerHTML = "<br>" + Players[currentPlayer];
+        boardState[slotNmbr] = Players[currentPlayer];
+        calculateWinner();
+    } else {
+        locked = false;
+    }
+}
+
 function drawWinningSlots() {
     winnigSlots.map((x) => document.getElementById("slot" + (x + 1)).setAttribute("style", "background-color: rgba(13, 218, 40, 0.87) !important"));
+}
+
+function isBoardEmpty() {
+    return !boardState.includes("X") && !boardState.includes("O");
 }
 
 function calculateRows() {
